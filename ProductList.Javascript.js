41a9a2897c998f90340f4d358dc8d4c9b62c8b27
Stephanie Caston <Stephanie.Caston@agdata.com>
2021-11-30 16:40:36 -0500

    updates

--- ProductList.Javascript.js
+++ ProductList.Javascript.js
@@ -63,14 +63,16 @@ function addCartItem(product, id) {
     clone.setAttribute('product-quantity', 1);
     clone.removeAttribute('id');
     
-    var fragment = document.createElement('span');
+    var fragment = document.createElement('div');
     fragment.setAttribute('class', 'product-quantity');
     fragment.innerHTML = ' x 1';
-    clone.appendChild(fragment);    
-    
-    fragment = document.createElement('span');
-    fragment.setAttribute('class', 'sub-total');
-    clone.appendChild(fragment);                    
+    node = clone.children[0].children[0].children[1];
+    node.insertBefore(fragment, node.children[6]);
+
+    fragment = document.createElement('div');
+    fragment.setAttribute('class', 'product-count');
+    node = clone.children[0].children[0].children[1];
+    node.insertBefore(fragment, node.children[7]);                  
     cart.appendChild(clone);
 }
 
@@ -78,27 +80,28 @@ function updateCartItem(product){
     var quantity = product.getAttribute('product-quantity');
     quantity = parseInt(quantity) + 1
     product.setAttribute('product-quantity', quantity);
-    var span = product.querySelectorAll('span.product-quantity');
-    span[0].innerHTML = ' x ' + quantity;
+    var div = product.querySelectorAll('div.product-quantity');
+    div[0].innerHTML = ' x ' + quantity;
 }
 
 function updateCart(){
-    var total = 0.0;
+    var subtotal = 0.0;
     var cart_items = document.querySelectorAll("div.cart ul li")
     for (var i = 0; i < cart_items.length; i++) {
         var cart_item = cart_items[i];
         var quantity = cart_item.getAttribute('product-quantity');
         var priceElement = cart_item.getElementsByClassName('product-price')[0]
         var price = parseFloat(priceElement.innerText.replace('$', ''));
-        var sub_total = parseFloat(quantity * price);
-        cart_item.querySelectorAll("span.sub-total")[0].innerHTML = " = $" + sub_total.toFixed(2);
+        var product_count = parseFloat(quantity * price);
+        cart_item.querySelectorAll("div.product-count")[0].innerHTML = " = $" + product_count.toFixed(2);
         
-        total += sub_total;
+        subtotal += product_count;
     }
     
-    var tax = (total * .07).toFixed(2);
-    total = (total + parseFloat(tax)).toFixed(2);
-    document.getElementsByClassName("tax")[0].innerText = 'Tax: $' + tax;
-    document.getElementsByClassName("total")[0].innerText = 'Total: $' + total;
+    var tax = subtotal * .07;
+    var total = subtotal + tax;
+    document.getElementById("subtotal").innerText = '$' + subtotal.toFixed(2);
+    document.getElementById("tax").innerText = '$' + tax.toFixed(2);
+    document.getElementById("total").innerText = '$' + total.toFixed(2);
 }
     
